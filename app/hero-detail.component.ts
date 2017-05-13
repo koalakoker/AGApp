3ce7a76d2c018342fecce66c5f156cb079b95ce6
koalakoker <koalakoker@gmail.com>
2017-05-13 14:58:57 +0200

    Basic form working

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component }              from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'hero-detail',
+  templateUrl: './hero-detail.component.html'
+})
+
+export class HeroDetailComponent {
+    heroForm = new FormGroup ({
+        name: new FormControl()
+    });
+}
