1a173904af37b9e98907d1c327edb189402395df
koalakoker <koalakoker@gmail.com>
2017-05-18 21:09:24 +0200

    Test

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports:      [ BrowserModule ],
