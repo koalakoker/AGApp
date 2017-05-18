ddd2f57926fb2a0d4b8ad5540964e5c348364d35
Koalakoker <koalakoker@gmail.com>
2017-05-18 07:14:43 +0200

    Included ngChart code - Not tested

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, ChartsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
