60ff1e124e6196b68eea83db2c3c6b6acfeb6581
koalakoker <koalakoker@gmail.com>
2017-05-20 22:15:15 +0200

    Overplotting chart

--- app/canv.component.ts
+++ app/canv.component.ts
@@ -6,35 +6,46 @@ import { Component, AfterViewChecked } from "@angular/core";
 })
 export class CanvComponent implements AfterViewChecked
 {
+    dataX : number[] = [0,50,100,150,200,250,300,350,400];
+    dataY : number[] = [300, 250, 200, 250, 250, 100, 150, 50, 20];
+    dataNum : number = 9;
+
     dbg = "Debug text";
 
     width = 400;
     height = 300;
 
+    canvas: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+
     click() : void
     {
         this.dbg = "Click";
-        this.show();
+        this.ctx.fillStyle = 'rgb(255,255,255)';
+        this.ctx.fillRect(0,0,this.width,this.height);
+        for (var i = 0; i < this.dataNum; i++) {
+            this.dataY[i] = Math.random() * this.height;
+        }
     }
 
     show() : void
     {
-        var canvas: HTMLCanvasElement;
-        var ctx: CanvasRenderingContext2D;
-
-        canvas = <HTMLCanvasElement>document.getElementById('myCanvas');
-        ctx = canvas.getContext("2d");
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = 'red';
-        ctx.moveTo(0,0);
-        ctx.lineTo(200,100);
-        ctx.lineTo(400,0);
-        ctx.moveTo(0,300);
-        ctx.lineTo(200,200);
-        ctx.lineTo(400,300);
-        ctx.moveTo(200,200);
-        ctx.lineTo(200,100);
-        ctx.stroke();
+        if (this.canvas == null)
+        {
+            this.canvas = <HTMLCanvasElement>document.getElementById('myCanvas');
+        }
+        if (this.ctx == null)
+        {
+            this.ctx = this.canvas.getContext("2d");
+        }
+        
+        this.ctx.lineWidth = 1;
+        this.ctx.strokeStyle = 'red';
+        this.ctx.moveTo(this.dataX[0],this.dataY[0]);
+        for (var i = 1; i < this.dataNum; i++) {
+            this.ctx.lineTo(this.dataX[i],this.dataY[i]);
+        }
+        this.ctx.stroke();
         
     }
 
