82a279af93cefcdcc7047e74060d3dff499d041b
koalakoker <koalakoker@gmail.com>
2017-05-20 14:36:48 +0200

    Plot using Google chart - working - Make use of .js

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { LineChartDemoComponent } from './line-chart-demo'
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>Google chart with: {{name}}</h1>
+  <div id="chart_div"></div>
+  `
+  ,
 })
 export class AppComponent  { name = 'Angular'; }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, ChartsModule ],
+  imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
